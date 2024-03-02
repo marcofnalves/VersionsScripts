5dee5fb16d3905d1be200f20f5346ee287687803
marcofnalves <33174320+marcofnalves@users.noreply.github.com>
2024-03-02 15:01:47 +0000

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -1,8 +1,8 @@
-Scripts = {}
-Scripts.Version = {}
-Scripts.Version.MafanModules = "1.0"
-Scripts.Version.MafanAnimations = "1.0"
-Scripts.Version.MafanTextUI = "1.0"
-Scripts.Version.MafanVip = "1.0"
-Scripts.Version.MafanVRPAddon = "1.0"
-Scripts.Version.MafanLeaderboard = "1.0"
+Scripts = {} 
+Scripts.Version = {} 
+Scripts.Version.MafanModules = "1.0" 
+Scripts.Version.MafanAnimations = "1.0" 
+Scripts.Version.MafanTextUI = "1.0" 
+Scripts.Version.MafanVip = "1.0" 
+Scripts.Version.MafanVRPAddon = "1.0" 
+Scripts.Version.MafanLeaderboard = "1.0" 
